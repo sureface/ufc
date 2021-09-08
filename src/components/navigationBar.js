dd40932b74c6057fd752e8bc0eefd1ac0211dc30
unknown <sureface>
2021-09-08 17:13:44 +0300

    music successfully finished

--- src/components/navigationBar.js
+++ src/components/navigationBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import {
     Collapse,
     Navbar,
@@ -13,9 +13,28 @@ import '../styles/home.scss'
 
  const NavigationBar = (props) => {
 
-    const [changeNav, setChangeNav] = useState(false)
-
+     const [changeNav, setChangeNav] = useState(false);
      const [isOpen, setIsOpen] = useState(false);
+
+     const [audio] = useState(new Audio('https://cdn1.sefon.pro/prev/Rv5XIkIIJlKyiYhKa2fZzg/1631125516/175/NEFFEX%20-%20Never%20Give%20Up%20%28192kbps%29.mp3'));
+     const [playing, setPlaying] = useState(true);
+
+     //https://mp3hub.best/download/vic-sage-feat-mike-stud-college.mp3
+     //https://muzoff.net/uploads/files/2018-06/1527848533_eminem-my-name-is.mp3
+     //https://cdn1.sefon.pro/prev/Rv5XIkIIJlKyiYhKa2fZzg/1631125516/175/NEFFEX%20-%20Never%20Give%20Up%20%28192kbps%29.mp3
+     //https://cdn1.sefon.pro/prev/6gI2V6e7l8q3KYWvBxzJAw/1631125392/95/Eminem%20-%20River%20%28192kbps%29.mp3
+     //https://static.muzlo.cc/download/36590/NEFFEX_-_Till-l-Collapse.mp3
+     //https://static.muzlo.cc/download/40371/NEFFEX_-_Fight-Back.mp3
+
+     useEffect(() => {
+         playing ? audio.play() : audio.pause();
+
+     }, [playing]);
+
+     const startPlay = () => {
+         setPlaying(!playing);
+         console.log(playing);
+     }
 
      const toggler = () => setIsOpen(!isOpen);
 
@@ -57,6 +76,7 @@ import '../styles/home.scss'
                          </NavItem>
                      </Nav>
                      <button type="button" className="btn btn-warning">Sign in</button>
+                     <button type="button" className="btn btn-danger" onClick={startPlay}>music</button>
                  </Collapse>
              </Navbar>
          </div>
